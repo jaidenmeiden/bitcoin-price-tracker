de7cb763741c4fd95599d1be1e8ca6c9223583fa
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-26 13:51:22 -0500

    feat: Create BitcoinCard

---
+++ src/components/BitcoinCard/BitcoinCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonSkeletonText } from '@ionic/react'
+
+import './BitcoinCard.css';
+
+const BitcoinCard = (props) => (
+    <IonCard>
+        <IonCardHeader>
+            <IonCardSubtitle>
+                {props.data.code}
+            </IonCardSubtitle>
+            <IonCardTitle>
+                {props.data.read_float}
+            </IonCardTitle>
+        </IonCardHeader>
+    </IonCard>
+);
+
+export default BitcoinCard;
