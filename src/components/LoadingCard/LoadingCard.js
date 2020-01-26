02c4bba6a6a14a96d14a102a070fe0a55b4fc03e
Jaiden Meiden <jaidenmeiden@gmail.com>
2020-01-26 10:21:03 -0500

    feta: Create LoadingCard component

---
+++ src/components/LoadingCard/LoadingCard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonSkeletonText } from '@ionic/react'
+
+import './LoadingCard.css';
+
+const LoadingCard = () => (
+    <IonCard>
+        <IonCardHeader>
+            <IonCardSubtitle>
+                <IonSkeletonText animated className="loading-card__currency-name"></IonSkeletonText>
+            </IonCardSubtitle>
+            <IonCardTitle>
+                <IonSkeletonText animated className="loading-card__currency-value"></IonSkeletonText>
+            </IonCardTitle>
+        </IonCardHeader>
+    </IonCard>
+);
